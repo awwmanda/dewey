f84147d209e8033d0c279159b626fc8adf362480
awwmanda <awwmanda@hotmail.com>
2017-05-27 23:36:54 +1000

    adding functions to support adding an image for new items

--- light/LibItemViewController.swift
+++ light/LibItemViewController.swift
@@ -9,10 +9,9 @@
 import UIKit
 import os.log
 
-class LibItemViewController: UIViewController, UITextFieldDelegate {
+class LibItemViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     //MARK: Properties
-    
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var bookNameLabel: UILabel!
     @IBOutlet weak var coverImageView: UIImageView!
@@ -29,6 +28,7 @@ class LibItemViewController: UIViewController, UITextFieldDelegate {
         nameTextField.delegate = self
         //enable the save button only if text field has a valid name
         updateSaveButtonState()
+        
     }
 
     //MARK: UITextFieldDelegate
@@ -65,15 +65,48 @@ class LibItemViewController: UIViewController, UITextFieldDelegate {
         let cover = coverImageView.image
         let userRating = ratingControl.rating
         
-        //set the meal to be passed to MealTableViewController after the unwind segue
+        //set the item to be passed to LibraryItemTableViewController after the unwind segue
         libraryitem = LibraryItem(title: title, itemType: "", author: "", userRating: userRating, isbn: "", inTopFive: false, currentItem: false, cover: cover, userReview: "")
     }
+    
+    //MARK: UIImagePickercontrollerDelegate
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        //dismiss the picker if user cancels
+        dismiss(animated: true, completion: nil)
+      }
+    
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]){
+            guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else{
+                fatalError("Expected a dictionary containing image, but was provided the following \(info)")
+            }
+            coverImageView.image = selectedImage
+            
+            //dismiss the picker
+            dismiss(animated: true, completion: nil)
+        }
     
     //MARK: Actions
     
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
         bookNameLabel.text = "Default Text"
     }
+    
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        //hide keyboard
+        nameTextField.resignFirstResponder()
+        
+        let imagePickerController = UIImagePickerController()
+        //only allow images to be picked not taken
+        imagePickerController.sourceType = .photoLibrary
+        
+        //make sure viewcontroller is notified when user picks an image
+        imagePickerController.delegate = self
+        
+        present(imagePickerController, animated: true, completion: nil)
+        
+    }
+    
+    
     //MARK: Private methods
     private func updateSaveButtonState(){
         //disable the save button if the text field is empty
